b290d6289d418e31477c049d9196d977467ace7d
Will Mainwaring <willm@yogainternational.com>
2019-06-26 23:44:58 -0400

    implement Bootstrap carousel with book info

--- client/src/Carousel.jsx
+++ client/src/Carousel.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 class BookCarousel extends React.Component {
   constructor(props) {
     super(props);
-  }
 
+    this.state = {
+      index: 0,
+      direction: null,
+    };
+
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
   render() {
+    const { index, direction } = this.state;
     const { books } = this.props;
     return (
-      <div>
+      <Carousel activeIndex={index} direction={direction} onSelect={this.handleSelect}>
         {books.map(book => (
-          <div>{book.title}</div>
+          <Carousel.Item key={book.id}>
+            <img className="d-block w-100" src={book.image} alt={book.title} />
+            <Carousel.Caption>
+              <h3>{book.title}</h3>
+              <p>{book.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
         ))}
-      </div>
+      </Carousel>
     );
   }
 }
